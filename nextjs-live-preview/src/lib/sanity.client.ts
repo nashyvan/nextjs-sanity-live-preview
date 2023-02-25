14287c7cc02e3fc2f497aeacbe1ca3958e55a50d
yariknashivan <yarik.nashivan@gmail.com>
2023-02-25 21:23:52 +0200

    fix string undefined

--- nextjs-live-preview/src/lib/sanity.client.ts
+++ nextjs-live-preview/src/lib/sanity.client.ts
@@ -2,9 +2,9 @@
 
 import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION; // "2023-01-01"
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
 
 export const client = createClient({
   projectId,
